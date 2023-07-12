ab2e3ab61456ebd35f939d4638a623a6c48a3e1e
Adeola <aadelakun28@gmail.com>
2023-07-12 13:41:56 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Challenge2-solana
-Building on Solana using Javascript - A close look at transactions and fees
+# Project Title
+
+Challenge2-solana
+
+## Description
+
+Building on Solana using Javascript - A close look at transactions and fees.
+
+## Getting Started
+
+* Clone this repository to get an exact copy of this program on your computer by running the following command.
+
+```
+git clone https://github.com/Adesdesk/Challenge2-solana.git
+```
+### Installing
+
+* Open the repository folder (now in your device) using an IDE like VSCode where you can simultaneously use the terminal.
+* In the terminal, ensure to be pointing at the project folder and run the following command
+```
+npm install
+```
+
+### Executing program
+
+Open the file "index.js" where you will find a section of code delimited within two sets of continous lines as follows.
+```
+// _________________________________________________________________________________________________________
+Block(s) of code...
+// _________________________________________________________________________________________________________
+```
+
+* The code block in the first of these sets of lines enables you create a wallet and derive its secret key (in the "DEMO_FROM_SECRET_KEY" array).
+* The code block in the second of these sets of lines implements the function "transferSol" which helps to derive a Keypair from its Secret Key, generate another Keypair - the receiving wallet, aidrop 2 SOL to Sender wallet, and send funds from the "from" wallet into "to" wallet.
+* The active code blocks outside the lines and comments implement a transaction that fetches the balance of the sender wallet, transfers 50% of that amount to the receiver wallet, and outputs coresponding wallet balances to the console.
+* In terminal, ensure that you are accessing this project folder 
+* Once in, run the following command, ensuring to understand and comment out the irrelevant blocks of code for each transaction you prefer to run at each instance. 
+
+```
+node index.js
+```
+
+## Help
+
+* You must have NodeJS installed to your PC to run this program as described.
+
+## Authors
+
+Contributor(s) names and contact info
+
+Name: Adeola David Adelakun  
+
+Email: aadelakun28@gmail.com
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
--- index.js
+++ index.js
@@ -11,22 +11,80 @@ const {
     sendAndConfirmTransaction
 } = require("@solana/web3.js");
 
-
+/*
+// __________________________________________________________________________________
 // Get the sender wallet and derive its secret key [array below] thus;
-// const _from = Keypair.generate();
-//     console.log('The new keypair is: ', _from.secretKey);
+const _from = Keypair.generate();
+    console.log('The new keypair is: ', _from.secretKey);
+// __________________________________________________________________________________
+*/
 
 const DEMO_FROM_SECRET_KEY = new Uint8Array(
     [
-        219, 176, 171, 130, 190, 250, 213,  32, 110,  46, 195,
-        188, 128, 202, 242, 213,  26, 251, 248,  40, 109, 140,
-          7, 117,  82,  14,  89,  65,  58,  49, 127,  29,  72,
-        166, 219,  80,  59, 180,  21, 242, 211,  43,  42,  14,
-         90,  73,  29, 250, 143, 150, 112, 117,  80,  82, 102,
-         67,  11, 200, 233, 121, 207,  56, 243,  27
-      ]            
+        13, 232, 188,  31, 197, 128,  71, 121,  96, 116, 245,
+        75,  16,  95,  79, 110,  72,  46,  88,  19, 251, 151,
+        36, 167, 171,  43,  25,  62, 182, 140, 196,  64, 107,
+        149, 239, 138,  85, 149, 172,  99,  68, 214, 205, 126,
+        114,  54,  33, 127,  90, 200, 149, 141,  86, 230, 119,
+        44, 204,  65,  88, 100,  76, 232,  56, 128
+        ]            
 );
 
+/*
+// __________________________________________________________________________________
+const transferSol = async() => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    // Get Keypair from Secret Key
+    var from = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
+
+    // Other things to try: 
+    // 1) Form array from userSecretKey
+    // const from = Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
+    // 2) Make a new Keypair (starts with 0 SOL)
+    // const from = Keypair.generate();
+
+    // Generate another Keypair (account we'll be sending to)
+    const to = Keypair.generate();
+
+    // Aidrop 2 SOL to Sender wallet
+    console.log("Airdopping some SOL to Sender wallet!");
+    const fromAirDropSignature = await connection.requestAirdrop(
+        new PublicKey(from.publicKey),
+        2 * LAMPORTS_PER_SOL
+    );
+
+    // Latest blockhash (unique identifer of the block) of the cluster
+    let latestBlockHash = await connection.getLatestBlockhash();
+
+    // Confirm transaction using the last valid block height (refers to its time)
+    // to check for transaction expiration
+    await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: fromAirDropSignature
+    });
+
+    console.log("Airdrop completed for the Sender account");
+
+    // Send money from "from" wallet and into "to" wallet
+    var transaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: from.publicKey,
+            toPubkey: to.publicKey,
+            lamports: LAMPORTS_PER_SOL / 100
+        })
+    );
+
+    // Sign transaction
+    var signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [from]
+    );
+    console.log('Signature is ', signature);
+// __________________________________________________________________________________
+}*/
 
 const transferSol = async () => {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -66,4 +124,6 @@ const transferSol = async () => {
     console.log("Recipient balance:", recipientBalance / LAMPORTS_PER_SOL, "SOL");
 };
 
+
+// invoke the transferSol() function
 transferSol();
